757c02e315cce9ccf840aa16552ee52c531baf32
Wouter-Michiel Vierdag <michiel.vierdag@embl.de>
2026-02-02 02:14:03 +0100

    partial cleanup

--- README.md
+++ README.md
@@ -1,4 +1,96 @@
-This repository provides an end-to-end workflow to go from raw microscopy images to SubCell classification results and interactive UMAP visualizations.
+## Overview
+This repository provides the end-to-end workflow used in the paper [here](to_be_decided).
+The workflow starts with raw image data, processes it and runs SubCell classicication. Results
+can be interactively visualized afterwards. The code is specific to the data used in the paper,
+but can be adjusted to your own data.
+
+For more information about SubCell, see the [SubCell paper](https://www.biorxiv.org/content/10.1101/2024.12.06.627299v2).
+
+## Expected directory structure and naming
+
+### Configs
+First there should be a `configs` directory. This directory contains configurations for the interactive
+visualization with bokeh:
+        
+- colormaps.json: explain more here
+- constant.yaml:
+- plot_config.yaml:
+- umap_config.yaml:
+
+### Image Data
+The raw image data should be stored in a directory called `data`, which is present in this project directory.
+In this directory, for running the workflow with default parameters, it is expected that there is a directory called 
+`raw`. This directory contains experiment directories with names of the user's choice, that should contain either
+'.lif' or '.tif', '.tiff' images. The dimension names and order of these images is expected to be `czyx`. The images
+can contain one or multiple scenes (Fields of View).
+
+## Getting started
+
+### Installing Pixi
+
+This project uses [pixi](https://pixi.sh) for dependency management. To install pixi, follow these steps
+(check installation instructions [here](https://pixi.prefix.dev/latest/installation/) in case of potential outdated instructions):
+
+#### Windows
+You can download and run the [installer](https://github.com/prefix-dev/pixi/releases/latest/download/pixi-x86_64-pc-windows-msvc.msi)
+or run the following:
+```
+# Using PowerShell (recommended)
+powershell -ExecutionPolicy ByPass -c "irm -useb https://pixi.sh/install.ps1 | iex"
+```
+The terminal needs to be restarted to make the installation effective. See details below for what this does.
+<details>
+The above invocation will automatically download the latest version of pixi, extract it, and move the pixi binary to 
+%UserProfile%\.pixi\bin. The command will also add %UserProfile%\.pixi\bin to your PATH environment variable, allowing 
+you to invoke pixi from anywhere.
+</details>
+
+If you want to turn on autocompletion check the location of your profile by running in the
+PowerShell: `$PROFILE`. At the end of this file add the following line:
+
+```(& pixi completion --shell powershell) | Out-String | Invoke-Expression```
+#### macOS/Linux
+```
+# Using curl
+curl -fsSL https://pixi.sh/install.sh | sh
+
+# Or using wget if you don't have curl
+wget -qO- https://pixi.sh/install.sh | sh
+```
+
+Now restart your terminal or shell to make the installation effective. For what this does, see the windows section.
+To enable autocompletion, add to your `.bashrc`:
+```eval "$(pixi completion --shell bash)"```
+
+### Installing Dependencies
+
+Once pixi is installed, you can install the project dependencies:
+
+```
+# Install all dependencies
+pixi install -a
+```
+
+This installs the dependencies for all environments related to this project.
+
+## Running the workflow
+
+### Overview
+#### Splitting images into 1 scene per image file
+The workflow starts from either raw `.lif` or `.tif` / `tiff` images. These are split into individual channel
+images by running a CLI. To run with default parameters, in the command line ensure that you are in this repository directory and run: 
+
+`pixi run split_images`
+
+Additional parameters can be provided:
+![split_lif](readme_images/split_help.png)
+
+Both `experiments` and `channels` can be a list of strings. The way to provide multiple channels for example is:
+`pixi run split_images -c ch1 -c ch2 -c ch3`
+
+#### Preprocessing of images
+
+
 
 Raw images (LIF / TIFF)
         ↓
--- scripts/split_lif.py
+++ scripts/split_lif.py
@@ -1,8 +1,13 @@
-import argparse
+import typer
 from bioio import BioImage
 import bioio_lif
 from bioio.writers import OmeTiffWriter
 from pathlib import Path
+from loguru import logger
+from tqdm import tqdm
+
+app = typer.Typer(help="Split LIF/TIFF files into per-scene OME-TIFFs.")
+
 
 def get_scene_name(image, img, scene, scene_index):
     if image.suffix.lower() in ('.tiff', '.tif'):  
@@ -17,17 +22,15 @@ def get_scene_name(image, img, scene, scene_index):
 
 
 def split_lif(experiments, input_dir, output_dir, channels):
-    print(f'\nSeparating scenes...')
-    
+    logger.info(f"Splitting image files of {len(experiments)} experiments.")
     for exp in experiments:
-        print(f'\nExperiment: {exp}')
+        logger.info(f'Splitting image file of experiment: {exp}')
         exp_dir = input_dir / exp
         images = exp_dir.iterdir()
         images = [image for image in images if image.suffix.lower() in ('.tiff', '.tif', '.lif')]
-        print(f"Found {len(images)} files in {exp}.")
+        logger.info(f"Found {len(images)} files in {exp}.")
 
         out_dir = output_dir / exp
-        # check if the output directory exists, if not create it
         out_dir.mkdir(parents=True, exist_ok=True)
 
         for i, image in enumerate(images):
@@ -38,63 +41,76 @@ def split_lif(experiments, input_dir, output_dir, channels):
 
             scenes = sorted(img.scenes)
             if len(scenes) > 1:
-                print(f"  Found {len(scenes)} scenes in {image}.")
+                logger.info(f"  Found {len(scenes)} scenes in {image}.")
 
-            for j, scene in enumerate(scenes):
-                scene_name = get_scene_name(image, img, scene, j)
+            for index, scene in tqdm(enumerate(scenes), total=len(scenes)):
+                scene_name = get_scene_name(image, img, scene, index)
                 scene_file = out_dir / f'{scene_name}.tif'
 
                 img.set_scene(scene)
-                print(f"  S {j}: {scene_name} - {img.shape}")
+                logger.info(f"  S {index}: {scene_name} - {img.shape}")
 
-                # get the image data
-                img_data = img.data     # Opened as TCZYX
-
-                if img_data.shape[0] != 1:
-                    raise NotImplementedError("Time dimension is not supported.")
-                img_data = img_data[0]  # Remove T dimension -> CZYX
-
-                # if a projection has already been done along Z, the shape will be C1YX, some images may have 1CYX
-                if img_data.shape[0] == 1:
-                    img_data = img_data[0]  # Remove Z dimension -> CYX
-                    dims = 'CYX'
-                elif img_data.shape[1] == 1:
+                img_data = img.get_image_data("CZYX")
+                if img_data.shape[1] == 1:
                     img_data = img_data[:, 0, :, :]  # Remove Z dimension -> CYX
                     dims = 'CYX'
                 else:
                     dims = 'CZYX'
 
-                print(f"Image data shape after removing T dimension: {img_data.shape}, dims: {dims}")
-
-                # check if the image has the correct number of channels
                 if img_data.shape[0] != len(channels):
-                    raise ValueError(f"Image has {img_data.shape[0]} channels, but expected {len(channels)} channels.")
-                print(f"Saving scene {j} with shape {img_data.shape} to {scene_file}")
+                    raise ValueError(f"Image has {img_data.shape[0]} channels, but  {len(channels)} channels were defined.")
+                logger.info(f"Saving scene {index} with shape {img_data.shape} to {scene_file}")
 
-                # Save the image
                 OmeTiffWriter.save(
                     img_data,
                     scene_file,
-                    dim_order=dims,  # must match your array shape
+                    dim_order=dims,
                     channel_names=channels,
                     image_name=scene_name,
                 )
 
 
+@app.command()
+def main(
+    input_dir: Path = typer.Option(
+        Path("data/raw"),
+        "--input-dir",
+        "-i",
+        help="Path to the input directory containing experiments. (default: data/raw)"
+    ),
+    experiments: list[str] = typer.Option(
+        [],
+        "--experiments",
+        "-e",
+        help="Experiment name to process (default: all subdirectories)."
+    ),
+    output_dir: Path = typer.Option(
+        Path("data/experiments"),
+        "--output-dir",
+        "-o",
+        help="Path to the output directory where resulting images are stored. (default: data/experiments)",
+
+    ),
+    channels: list[str] = typer.Option(
+        [
+            "DAPI", "TOMM20", "alphaTUBULIN", "SC35", "SP100", "WGA", "SON",
+            "VIMENTIN", "LAMP1", "COILIN", "GM130", "G3BP1", "TFAM",
+            "Ki67", "NPM1"
+        ],
+        "--channels",
+        "-c",
+        help="List of channel names. Length should match with the number of channels in the images."
+    ),
+):
+    print(channels)
+    input_dir = input_dir.resolve()
+    output_dir = output_dir.resolve()
+
+    if not experiments:
+        experiments = [p.name for p in input_dir.iterdir() if p.is_dir()]
+
+    split_lif(experiments, input_dir, output_dir, channels)
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Split LIF files")
-    parser.add_argument("--input_dir", help="Path to the input LIF file", type=str, default='raw')
-    parser.add_argument("--experiment", help="Experiment name to filter files", type=str, default=None)
-    parser.add_argument("--output_dir", help="Directory to save the split files", type=str, default='experiments')
-    parser.add_argument("--channels", help="List of channel names", type=str, nargs='+', default=["DAPI", "TOMM20", "alphaTUBULIN", "SC35", "SP100", "WGA", "SON", "VIMENTIN", "LAMP1", "COILIN", "GM130", "G3BP1", "TFAM", "Ki67", "NPM1"])
-    args = parser.parse_args()
-
-    input_dir = Path(args.input_dir)
-    output_dir = Path(args.output_dir)
-
-    # get the list of experiments
-    experiments = [args.experiment] if args.experiment else [p.name for p in input_dir.iterdir()]
-    # check that all elements in experiments are directories
-    experiments = [exp for exp in experiments if (input_dir / exp).is_dir()]
-    # split the files for each experiment
-    split_lif(experiments, input_dir, output_dir, args.channels)
+    app()
